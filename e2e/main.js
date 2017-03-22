47bb23343378f03416231beebbd48b8f64266a7e
roll <eskarev@gmail.com>
2017-03-22 19:35:11 +0300

    recovered e2e tests

--- e2e/main.js
+++ e2e/main.js
@@ -3,9 +3,7 @@ export default {
     client
       .url(client.launch_url)
       .waitForElementVisible('.goodtables-vue-report', 5000)
-      .assert.containsText('h1', 'Report')
-      .click('h3.error')
-      .waitForElementVisible('thead', 1000)
+      .assert.containsText('a', 'data/invalid.csv')
       .end();
   },
   afterEach: (client, done) => {
