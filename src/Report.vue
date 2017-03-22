72c48fe7400128b5a9c27876199f5552c0678f0a
roll <eskarev@gmail.com>
2017-03-22 17:59:36 +0300

    implemented update design

--- src/Report.vue
+++ src/Report.vue
@@ -18,8 +18,8 @@ export default {
     <li v-for="error of report.errors">{{ error.message }}</li>
   </ul>
 
-  <template v-for="(table, index) of report.tables">
-    <MyTable :table="table" :tableCount="report['table-count']" :tableNumber="index + 1" />
+  <template v-for="(table, index) of report.tables" v-if="!table.valid">
+    <MyTable :table="table" :tablesCount="report['table-count']" :tableNumber="index + 1" />
   </template>
 
 </div>
--- src/Table.vue
+++ src/Table.vue
@@ -1,14 +1,62 @@
 <script>
-import Error from './Error.vue'
+import ErrorGroup from './ErrorGroup.vue'
 
 export default {
   props: {
     table: Object,
-    tableCount: Number,
     tableNumber: Number,
+    tablesCount: Number,
   },
   components: {
-    Error,
+    ErrorGroup,
+  },
+  computed: {
+    errorGroups() {
+      const groups = {}
+      for (let error of this.table.errors) {
+
+        // Get group
+        let group = groups[error.code]
+
+        // Create group
+        if (!group) {
+          group = {
+            name: error.code,
+            type: 'type',
+            rows: {},
+            count: 0,
+          }
+        }
+
+        // Get row
+        let row = group.rows[error['row-number']]
+
+        // Create row
+        if (!row) {
+          let values = error.row
+          if (!error['row-number']) values = this.table.headers
+          if (error['code'] === 'blank-row') values = this.table.headers.map(() => '')
+          row = {
+            values,
+            badcols: new Set(),
+          }
+        }
+
+        // Add row badcols
+        if (error['column-number']) {
+          row.badcols.add(error['column-number'])
+        } else {
+          row.badcols = new Set(row.values.map((value, index) => index + 1))
+        }
+
+        // Save group
+        group.count += 1
+        group.rows[error['row-number']] = row
+        groups[error.code] = group
+
+      }
+      return groups
+    },
   },
 }
 </script>
@@ -19,11 +67,11 @@ export default {
   <h4 class="table-heading">
     <span>
       <a class="table-name">{{ table.source }}</a>
-      <a class="table-count">File {{ tableNumber }} of {{ tableCount }}</a>
+      <a class="table-count">Table {{ tableNumber }} of {{ tablesCount }}</a>
     </span>
   </h4>
 
-  <Error name="name" type="type" :count="1">
+  <ErrorGroup v-for="errorGroup of errorGroups" :errorGroup="errorGroup" />
 
 </div>
 </template>
